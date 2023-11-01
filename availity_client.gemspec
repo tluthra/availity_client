f0da2b08a902c7a89f5bdc3d4e37a29e32c3dfbf
Tanooj Luthra <tanoojl@gmail.com>
2023-10-31 20:06:51 -0400

    Bump faraday version, fix authentication

--- availity_client.gemspec
+++ availity_client.gemspec
@@ -1,19 +1,19 @@
-require './lib/availity_client/version'
+require "./lib/availity_client/version"
 
 Gem::Specification.new do |s|
-  s.name        = 'availity_client'
-  s.version     = AvailityClient::VERSION
-  s.date        = '2016-05-11'
-  s.summary     = "Availity API REST Client"
+  s.name = "availity_client"
+  s.version = AvailityClient::VERSION
+  s.date = "2016-05-11"
+  s.summary = "Availity API REST Client"
   s.description = "HTTP wrapper for Availity API"
-  s.authors     = ["Nicholas Lee"]
-  s.email       = 'nicholas.lee.3@gmail.com'
-  s.files       = ["lib/availity_client.rb", "lib/availity_client/coverage.rb"]
-  s.homepage    = 'https://github.com/leeacto/availity_client'
-  s.license       = 'MIT'
+  s.authors = ["Nicholas Lee"]
+  s.email = "nicholas.lee.3@gmail.com"
+  s.files = ["lib/availity_client.rb", "lib/availity_client/coverage.rb"]
+  s.homepage = "https://github.com/leeacto/availity_client"
+  s.license = "MIT"
 
-  s.add_dependency 'faraday', '~> 0.9.0'
-  s.add_dependency 'require_all'
+  s.add_dependency "faraday", "~> 2.7.11"
+  s.add_dependency "require_all"
 
   s.add_development_dependency "bundler"
   s.add_development_dependency "rake"
--- lib/availity_client.rb
+++ lib/availity_client.rb
@@ -1,8 +1,8 @@
-require 'require_all'
-require 'faraday'
-require 'json'
-require 'uri'
-require 'base64'
+require "require_all"
+require "faraday"
+require "json"
+require "uri"
+require "base64"
 
 require_rel "./"
 
@@ -11,46 +11,44 @@ module AvailityClient
 
   class << self
     def base_url
-      "https://api.availity.com/#{premium_key? ? '' : 'demo/'}v1/"
+      "https://api.availity.com/#{premium_key? ? "" : "demo/"}v1/"
     end
 
     def premium_key?
-      !!ENV['AVAILITY_API_KEY'] && !!ENV['AVAILITY_API_SECRET']
+      !!ENV["AVAILITY_API_KEY"] && !!ENV["AVAILITY_API_SECRET"]
     end
 
     def generate_token
-      raise MissingApiKeyError, "AVAILITY_API_KEY and AVAILITY_API_SECRET  Environment Variables must be set" unless ENV['AVAILITY_API_KEY'] && ENV['AVAILITY_API_SECRET']
+      raise MissingApiKeyError, "AVAILITY_API_KEY and AVAILITY_API_SECRET  Environment Variables must be set" unless ENV["AVAILITY_API_KEY"] && ENV["AVAILITY_API_SECRET"]
 
-      base_64_token = Base64.encode64(ENV['AVAILITY_API_KEY'] + ":" + ENV['AVAILITY_API_SECRET']).chomp
       conn = Faraday.new(url: "https://api.availity.com") do |faraday|
         faraday.response :logger
-        faraday.adapter  Faraday.default_adapter
+        faraday.adapter Faraday.default_adapter
       end
 
       response = conn.post do |req|
-        req.url '/v1/token'
-        req.headers['Authorization'] = "Basic #{base_64_token}"
-        req.body = "grant_type=client_credentials"
+        req.url "/v1/token"
+        req.body = "grant_type=client_credentials&client_id=#{ENV["AVAILITY_API_KEY"]}&client_secret=#{ENV["AVAILITY_API_SECRET"]}&scope=hipaa"
       end
 
-      JSON.parse(response.body)['access_token']
+      JSON.parse(response.body)["access_token"]
     end
 
     def issue_request(method, url, params)
-      api_key = ENV['AVAILITY_API_KEY']
+      api_key = ENV["AVAILITY_API_KEY"]
       raise MissingApiKeyError, "AVAILITY_API_KEY Environment Variable must be set" unless api_key
 
       uri = URI.parse(url)
       conn = Faraday.new(url: uri) do |faraday|
         faraday.response :logger
-        faraday.adapter  Faraday.default_adapter
+        faraday.adapter Faraday.default_adapter
       end
 
       response = conn.send(method.to_sym) do |req|
         if premium_key?
-          req.headers['Authorization'] = "Bearer #{generate_token}"
+          req.headers["Authorization"] = "Bearer #{generate_token}"
         else
-          req.headers['x-api-key'] = api_key
+          req.headers["x-api-key"] = api_key
         end
 
         if params.kind_of?(Hash)
@@ -59,14 +57,14 @@ module AvailityClient
       end
 
       response_body = begin
-                        JSON.parse(response.body)
-                      rescue JSON::ParserError => e
-                        response.body
-                      end
+          JSON.parse(response.body)
+        rescue JSON::ParserError => e
+          response.body
+        end
       {
         headers: response.headers,
         status: response.status,
-        body: response_body
+        body: response_body,
       }
     end
   end
